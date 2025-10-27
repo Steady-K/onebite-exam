0aa4dd8ff7af3acf2c855bd830945f7973147a25
Steady-K <tkdlxm56@gmail.com>
2025-10-27 11:00:16 +0900

    docs: add shadcn/ui practice notes and example components(2)

--- src/App.tsx
+++ src/App.tsx
@@ -5,11 +5,94 @@ import { Input } from "./components/ui/input";
 import { Toaster } from "./components/ui/sonner";
 import { Textarea } from "./components/ui/textarea";
 import { cn } from "./lib/utils";
+import {
+  Carousel,
+  CarouselContent,
+  CarouselItem,
+  CarouselNext,
+  CarouselPrevious,
+} from "./components/ui/carousel";
+import { Popover, PopoverContent } from "./components/ui/popover";
+import { PopoverTrigger } from "@radix-ui/react-popover";
+import {
+  Dialog,
+  DialogContent,
+  DialogDescription,
+  DialogHeader,
+  DialogTitle,
+  DialogTrigger,
+} from "./components/ui/dialog";
+import {
+  AlertDialog,
+  AlertDialogAction,
+  AlertDialogCancel,
+  AlertDialogContent,
+  AlertDialogDescription,
+  AlertDialogFooter,
+  AlertDialogHeader,
+  AlertDialogTitle,
+  AlertDialogTrigger,
+} from "./components/ui/alert-dialog";
+
+import { Carrot, CarrotIcon } from "lucide-react";
 
 function App() {
   const isActive = true;
   return (
     <div className="p-5">
+      <Carrot className="h-10 w-10 fill-orange-500" />
+      <AlertDialog>
+        <AlertDialogTrigger>Alert Dialog</AlertDialogTrigger>
+        <AlertDialogContent>
+          <AlertDialogTitle>Title</AlertDialogTitle>
+          <div>body</div>
+          <AlertDialogAction
+            onClick={() => {
+              console.log("Action!");
+            }}
+          >
+            Continue
+          </AlertDialogAction>
+          <AlertDialogCancel
+            onClick={() => {
+              console.log("Cancel!");
+            }}
+          >
+            Cancel
+          </AlertDialogCancel>
+        </AlertDialogContent>
+      </AlertDialog>
+
+      <Dialog>
+        <DialogTrigger>Open Dialog</DialogTrigger>
+        <DialogContent>
+          <DialogHeader>
+            <DialogTitle>Title</DialogTitle>
+            <DialogDescription>Description</DialogDescription>
+          </DialogHeader>
+          <div>body</div>
+        </DialogContent>
+      </Dialog>
+
+      <Popover>
+        <PopoverTrigger asChild>
+          <Button>Open</Button>
+        </PopoverTrigger>
+        <PopoverContent>Content!</PopoverContent>
+      </Popover>
+
+      <Carousel className="mx-10">
+        <CarouselContent>
+          <CarouselItem className="basis-1/3">1</CarouselItem>
+          <CarouselItem className="basis-1/3">2</CarouselItem>
+          <CarouselItem className="basis-1/3">3</CarouselItem>
+          <CarouselItem className="basis-1/3">4</CarouselItem>
+          <CarouselItem className="basis-1/3">5</CarouselItem>
+        </CarouselContent>
+        <CarouselPrevious />
+        <CarouselNext />
+      </Carousel>
+
       <Toaster />
       <Textarea />
 

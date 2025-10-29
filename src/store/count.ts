562476518ddc3f359240f14988cdc114f3039ee2
Steady-K <tkdlxm56@gmail.com>
2025-10-29 13:07:30 +0900

    feat(zustand): practice subscribeWithSelector middleware usage

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,26 +1,37 @@
 import { create } from "zustand";
-import { combine } from "zustand/middleware";
+import { combine, subscribeWithSelector } from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 export const useCountStore = create(
-  immer(
-    combine({ count: 0 }, (set, get) => ({
-      actions: {
-        increaseOne: () => {
-          set((state) => {
-            state.count += 1;
-          });
+  subscribeWithSelector(
+    immer(
+      combine({ count: 0 }, (set, get) => ({
+        actions: {
+          increaseOne: () => {
+            set((state) => {
+              state.count += 1;
+            });
+          },
+          decreaseOne: () => {
+            set((state) => {
+              state.count -= 1;
+            });
+          },
         },
-        decreaseOne: () => {
-          set((state) => {
-            state.count -= 1;
-          });
-        },
-      },
-    })),
+      })),
+    ),
   ),
 );
+useCountStore.subscribe(
+  (store) => store.count,
+  (count, prevCount) => {
+    // Lisner
+    console.log(count, prevCount);
 
+    const store = useCountStore.getState();
+    useCountStore.setState((store) => ({}));
+  },
+);
 // export const useCountStore = create<Store>((set, get) => ({
 //   count: 0,
 //   actions: {

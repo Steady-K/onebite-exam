3abde11bdf91c7b655b34e8794ff7c01207ccf60
Steady-K <tkdlxm56@gmail.com>
2025-10-29 13:43:02 +0900

    feat(zustand): practice persist middleware for state storage

--- src/store/count.ts
+++ src/store/count.ts
@@ -1,25 +1,39 @@
 import { create } from "zustand";
-import { combine, subscribeWithSelector } from "zustand/middleware";
+import {
+  combine,
+  subscribeWithSelector,
+  persist,
+  createJSONStorage,
+} from "zustand/middleware";
 import { immer } from "zustand/middleware/immer";
 
 export const useCountStore = create(
-  subscribeWithSelector(
-    immer(
-      combine({ count: 0 }, (set, get) => ({
-        actions: {
-          increaseOne: () => {
-            set((state) => {
-              state.count += 1;
-            });
+  persist(
+    subscribeWithSelector(
+      immer(
+        combine({ count: 0 }, (set, get) => ({
+          actions: {
+            increaseOne: () => {
+              set((state) => {
+                state.count += 1;
+              });
+            },
+            decreaseOne: () => {
+              set((state) => {
+                state.count -= 1;
+              });
+            },
           },
-          decreaseOne: () => {
-            set((state) => {
-              state.count -= 1;
-            });
-          },
-        },
-      })),
+        })),
+      ),
     ),
+    {
+      name: "countStore",
+      partialize: (store) => ({
+        count: store.count,
+      }),
+      storage: createJSONStorage(() => sessionStorage),
+    },
   ),
 );
 useCountStore.subscribe(
@@ -29,7 +43,7 @@ useCountStore.subscribe(
     console.log(count, prevCount);
 
     const store = useCountStore.getState();
-    useCountStore.setState((store) => ({}));
+    // useCountStore.setState((store) => ({}));
   },
 );
 // export const useCountStore = create<Store>((set, get) => ({

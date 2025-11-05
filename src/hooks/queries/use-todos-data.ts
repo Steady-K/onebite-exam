7f9ae6304a9a86088e7a2694d8b0687337cd5759
Steady-K <tkdlxm56@gmail.com>
2025-11-05 12:08:17 +0900

    refactor(cache): Todo 캐시 정규화 적용 - todo.list에는 id 배열만 저장 - 각 todo.detail 캐시에 개별 todo 데이터 분리 저장 - 중복 제거 및 데이터 일관성 개선

--- src/hooks/queries/use-todos-data.ts
+++ src/hooks/queries/use-todos-data.ts
@@ -1,10 +1,19 @@
 import { fetchTodos } from "@/api/fetch-todos";
 import { Query_Keys } from "@/lib/constants";
-import { useQuery } from "@tanstack/react-query";
-
+import type { Todo } from "@/types";
+import { useQuery, useQueryClient } from "@tanstack/react-query";
 export function useTodosData() {
+  const queryClient = useQueryClient();
   return useQuery({
-    queryFn: fetchTodos,
+    queryFn: async () => {
+      const todos = await fetchTodos();
+
+      todos.forEach((todo) => {
+        queryClient.setQueryData<Todo>(Query_Keys.todo.detail(todo.id), todo);
+      });
+
+      return todos.map((todo) => todo.id);
+    },
     queryKey: Query_Keys.todo.list,
   });
 }
